bc7871f6aa7fe615428e318ee35854b5fd75dc47
Alejandro Cruz <vidriloco@gmaikl.com>
2015-03-29 01:49:44 -0600

    Added more info modal

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -6,6 +6,26 @@ $(document).ready(function() {
 			play: 6000,
 			pagination: false
     });
+    
+    setTimeout(function() {
+      $('.fixed-container').fadeIn('slow');
+    }, 500);
+    
+    var showMoreInfo = function() {
+      $('.floating-navbar').hide();
+      $('.fixed-container').hide();
+      $('.fixed-modal').fadeIn('slow');
+    }
+    
+    var hideMoreInfo = function() {
+      $('.floating-navbar').fadeIn();
+      $('.fixed-container').fadeIn();
+      $('.fixed-modal').hide();
+    }
+    
+    $('.show-more-info').on('click', showMoreInfo);
+    $('.hide-more-info').on('click', hideMoreInfo);
+    
 	}
 	
 });
